13bbbd22d2e0860afe023326c4c3a324acf62405
lc <lc@fccs.com>
2022-10-09 11:30:27 +0800

    updata: config

---
+++ utiils/thousandNumber/index.js
@@ -0,0 +1,14 @@
+export function handleThousandNumber(value, groupSeparator = ",") {
+    const val = String(value);
+    const cells = val.match(/^(-?)(\d*)(\.(\d+))?$/);
+    if (!cells) {
+        return val;
+    }
+    else {
+        const negative = cells[1];
+        let int = cells[2] || "0";
+        const decimal = cells[3] || "";
+        int = int.replace(/\B(?=(\d{3})+(?!\d))/g, groupSeparator);
+        return negative + int + decimal;
+    }
+}
