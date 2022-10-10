f567afeb765abc794fc9bfea813c2452283f301d
for <eternallightwang@qq.com>
2022-10-10 13:57:12 +0800

    fix: typescript 默认优先引入 js 文件的问题

--- utils/test/thousandNumber/index.test.ts
+++ utils/test/thousandNumber/index.test.ts
@@ -1,7 +1,7 @@
 // import { describe, test, expect } from "vitest";
-import { handleThousandNumber } from "./index";
+import { handleThousandNumber, formatWithMod } from "./index";
 
-describe("数字千分位处理", () => {
+describe("数字千分位处理 - handleThousandNumber", () => {
   test("int test", () => {
     const result = handleThousandNumber(123456);
     expect(result).toBe("123,456");
@@ -11,3 +11,19 @@ describe("数字千分位处理", () => {
     expect(result).toBe("123,456.12399");
   });
 });
+
+describe("数字千分位处理 - formatWithMod", () => {
+  test("int test", () => {
+    const result = formatWithMod(123456);
+    expect(result).toBe("123,456");
+  });
+  test("float test", () => {
+    const result = formatWithMod(123456.12399);
+    expect(result).toBe("123,456.12399");
+  });
+});
+
+// formatWithMod(12365896) // '12,365,896'
+// formatWithMod(12365896.123) // '12,365,896.123'
+// formatWithMod(0.123) // '0.123'
+// formatWithMod(-10111.123)  // '-10,111.123'
