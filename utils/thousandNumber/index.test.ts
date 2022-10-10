70ade019d86793dcfe439c20aa702c6532ba2602
lc <lc@fccs.com>
2022-10-10 11:41:00 +0800

    feat: 浮点数测试

--- utils/thousandNumber/index.test.ts
+++ utils/thousandNumber/index.test.ts
@@ -4,6 +4,10 @@ import { handleThousandNumber } from "./index";
 describe("数字千分位处理", () => {
   test("int test", () => {
     const result = handleThousandNumber(123456);
-    expect(result).toBe("123,4561");
+    expect(result).toBe("123,456");
+  });
+  test("float test", () => {
+    const result = handleThousandNumber(123456.12399);
+    expect(result).toBe("123,456.12399");
   });
 });
