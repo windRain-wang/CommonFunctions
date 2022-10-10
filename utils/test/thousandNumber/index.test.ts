15ff552ea75d90169cb1cbc42ff19846858a7226
for <eternallightwang@qq.com>
2022-10-10 14:18:19 +0800

    fix: 修改 test 测试用例

--- utils/test/thousandNumber/index.test.ts
+++ utils/test/thousandNumber/index.test.ts
@@ -1,29 +1,29 @@
 // import { describe, test, expect } from "vitest";
-import { handleThousandNumber, formatWithMod } from "./index";
+import * as funcs from "./index";
 
-describe("数字千分位处理 - handleThousandNumber", () => {
-  test("int test", () => {
-    const result = handleThousandNumber(123456);
-    expect(result).toBe("123,456");
-  });
-  test("float test", () => {
-    const result = handleThousandNumber(123456.12399);
-    expect(result).toBe("123,456.12399");
-  });
-});
+const funcClone: any = funcs;
 
-describe("数字千分位处理 - formatWithMod", () => {
-  test("int test", () => {
-    const result = formatWithMod(123456);
-    expect(result).toBe("123,456");
+for (const key in funcClone) {
+  describe("数字千分位处理 - " + key, () => {
+    test("整数", () => {
+      const result = funcClone[key](123456);
+      expect(result).toBe("123,456");
+    });
+    test("小数", () => {
+      const result = funcClone[key](123456.12399);
+      expect(result).toBe("123,456.12399");
+    });
+    test("negative float test", () => {
+      const result = funcClone[key](-123456.12399);
+      expect(result).toBe("-123,456.12399");
+    });
+    test("zero point test", () => {
+      const result = funcClone[key](0.12399);
+      expect(result).toBe("0.12399");
+    });
+    test("four digit point test", () => {
+      const result = funcClone[key](1236);
+      expect(result).toBe("1,236");
+    });
   });
-  test("float test", () => {
-    const result = formatWithMod(123456.12399);
-    expect(result).toBe("123,456.12399");
-  });
-});
-
-// formatWithMod(12365896) // '12,365,896'
-// formatWithMod(12365896.123) // '12,365,896.123'
-// formatWithMod(0.123) // '0.123'
-// formatWithMod(-10111.123)  // '-10,111.123'
+}
